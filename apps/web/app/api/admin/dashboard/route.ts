5a7bcac2db3bfa95d0a09e6482213568e1a4b60b
M Zidane Sc <zidankren02@gmail.com>
2025-09-22 21:56:50 +0700

    remove type from import

--- apps/web/app/api/admin/dashboard/route.ts
+++ apps/web/app/api/admin/dashboard/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import type { Prisma } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 import prisma from '../../../../lib/prisma';
 import { getSession } from '../../../../lib/session';
 import { hasActiveSession } from '../../../../lib/auth';
